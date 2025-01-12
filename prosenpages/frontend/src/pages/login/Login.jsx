cf18a5d1b0a207fc9b0006fff052b2411f155200
ProseAndPages4Life <prosenpages4life@gmail.com>
2025-01-11 20:10:36 -0600

    Added error maping in frontend at login

--- prosenpages/frontend/src/pages/login/Login.jsx
+++ prosenpages/frontend/src/pages/login/Login.jsx
@@ -4,14 +4,14 @@ import { useAuth } from "../../context/authContext";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const { register, handleSubmit, formState:{
+  const { register, handleSubmit, formState: {
     errors
   } } = useForm();
-  const { loginUser, user, isAutenticado } = useAuth();
+  const { loginUser, user, isAutenticado, errorsLogin } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (isAutenticado) navigate("/")
+    if (isAutenticado) navigate("/");
   }, [isAutenticado]);
 
 
@@ -24,6 +24,13 @@ function Login() {
 
   return (
     <div className="form">
+      {
+        errorsLogin.map((error, i) => (
+          <div key={error}>
+            {error}
+          </div>
+        ))
+      }
       <form onSubmit={onSubmit}>
         <label htmlFor="Email">Correo electrónico: </label>
         <input type="text" {...register("Email", { required: true })} /><br /><br />
@@ -37,12 +44,12 @@ function Login() {
         <label htmlFor="Contraseña">Contraseña: </label>
         {/* <input type="text" {...register("Contraseña", { required: true })} /><br /><br /> */}
         <input type="password" {...register("Contraseña", { required: true })} /><br /><br />
-    
+
         <div className="libros">
           <div className="libros">
             <a>
               <button type="submit" className="botonRegistro"><h2>Entra!</h2></button>
-              
+
             </a>
           </div>
         </div>
